c14f5be5ead1acbc6dd4068ed7cda9717cbc5b05
Mosses <mosses@cloudiotech.com>
2024-10-28 15:05:41 -0400

    Added Emi table functions

--- src/FileCabinet/SuiteScripts/sri-mos-pc-mod-v1.js
+++ src/FileCabinet/SuiteScripts/sri-mos-pc-mod-v1.js
@@ -9,6 +9,15 @@ function main(queryModule) {
     return cts;
 }
 
+cts.status = {
+    PC_STATUS_PENDING_APPROVEL: 1,
+    PC_STATUS_APPROVED: 2,
+    PC_STATUS_REJECTD: 3,
+    PC_STATUS_DRAFT: 4
+}
+
+
+
 cts.updateCostAndMargin = (curRecord) => {
     var retValue = { success: false, message: '', data: {} };
     try {
@@ -21,10 +30,11 @@ cts.updateCostAndMargin = (curRecord) => {
         var defaultMargin = curRecord.getValue('custrecord_mos_pc_defa_margin_pc');
         defaultMargin = defaultMargin ? defaultMargin : 0;
         defaultMargin = parseFloat(defaultMargin);
-        log.debug({ title: 'Work Experience|Rate|Default Margin', details: workExperience + '|' + ratePerHour + '|' + defaultMargin });
+        log.debug({ title: 'Work Experience | Rate| Default Margin', details: workExperience + ' | ' + ratePerHour + ' | ' + defaultMargin });
 
         var result = computeCostAndMargin(workExperience, ratePerHour, defaultMargin);
         if (result.success) {
+
             curRecord.setValue({
                 fieldId: 'custrecord_mos_pc_cost_per_hour',
                 value: result.data.costPerHour
@@ -38,7 +48,7 @@ cts.updateCostAndMargin = (curRecord) => {
             throw result.message;
         }
         retValue.success = true;
-        retValue.message = 'Cost and Margin updated successfully';
+        retValue.message = 'Cost and Margin updated Successfully';
         retValue.data = { costPerHour: result.data.costPerHour, marginPercentage: result.data.marginPercentage };
     } catch (e) {
         log.error({ title: 'Error', details: e });
@@ -52,13 +62,13 @@ cts.updateCostAndMargin = (curRecord) => {
 function computeCostAndMargin(workExperience, ratePerHour, defaultMargin) {
     var retValue = { success: false, message: '', data: {} };
     try {
-        var marginPercentage = 20;
+        var marginPercentage = 0;
         if (workExperience >= 1 && workExperience <= 3) {
-            marginPercentage = 15;
-        } else if (workExperience > 3 && workExperience <= 5) {
             marginPercentage = 10;
+        } else if (workExperience > 3 && workExperience <= 5) {
+            marginPercentage = 15;
         } else if (workExperience > 5 && workExperience <= 10) {
-            marginPercentage = 5;
+            marginPercentage = 20;
         } else {
             marginPercentage = defaultMargin;
         }
@@ -81,25 +91,130 @@ function computeCostAndMargin(workExperience, ratePerHour, defaultMargin) {
     return retValue;
 }
 
+// Creating a task 
 cts.createTask = (curRecord) => {
-    log.debug("Create task function")
+    var retValue = { success: false, message: '', data: {} };
     try {
         var ConsultantFullName = curRecord.getValue('name');
         var task = record.create({ type: record.Type.TASK });
-        var subject = 'Follow up regarding onboarding of ' + ConsultantFullName;
+        log.debug({ title: "task json", details: task })
+        var subject = 'Fellow up regarding onboarding to ' + ConsultantFullName;
         task.setValue({
             fieldId: 'title', value: subject
         });
+        // 4376
         task.setValue({
-            fieldId: 'assigned', value: 4345
+            fieldId: 'assigned', value: 4376
         });
         task.setValue({
-            fieldId: 'message', value: "Please verify!"
+            fieldId: 'priority', value: "HIGH"
+        });
+        task.setValue({
+            fieldId: 'status', value: "COMPLETE"
+        });
+        task.setValue({
+            fieldId: 'message', value: "Please verify the consultant record and request for documents"
         });
         var taskId = task.save();
-
+        log.debug({ title: "taskId", details: taskId })
+        retValue.success = true;
+        retValue.message = 'The Task is created Successfully';
     }
     catch (e) {
         log.error({ title: 'Error', details: e });
+        retValue.success = false;
+        retValue.message = e.message;
+        retValue.data = {};
     }
+    return retValue;
+}
+
+cts.calculateSimpleInterest = (principal, rate, time) => {
+    rate = rate * 0.1;
+    return (principal * rate * time) / 100;
+}
+
+
+cts.calculateCompoundInterest=(principal, rate, time, n)=>{
+    rate = rate /100;
+    let amount = principal * Math.pow((1 + rate / n), n * time);
+    let Interest = amount - principal;
+    return Interest.toFixed(2);
+}
+
+cts.calculateEMI=(P, annualInterestRate, loanTenureYears)=> {
+    let monthlyInterestRate = (annualInterestRate / 100) / 12;
+    let loanTenureMonths = loanTenureYears * 12;
+    let EMI = (P * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, loanTenureMonths)) / (Math.pow(1 + monthlyInterestRate, loanTenureMonths) - 1);
+    return EMI;
+}
+
+
+cts.GenerateHTML=(scriptContext, myParams)=>{
+    var retValue = { success: false, message: '', data: {} };
+    try {
+        let principal = myParams.data.principal;
+        let rate = myParams.data.rata
+        let time = myParams.data.time
+        let interest = myParams.data.interest
+        let Installment = myParams.data.emi
+        var Balance = principal
+        var curprincipal = principal;
+        var totalInterest=null;
+        var totalInstallment=null;
+        var innerHTML=""
+        innerHTML = `<html>
+    <body>
+        <table style="width: 100%;border-collapse: collapse;margin-top:25px;position: relative;left: 50%">
+            <thead style="background:#1c7e89">
+                <tr style="color: white;">
+                    <th style="border: 2px solid #111;padding: 11px;text-align: center;font-weight:900">S.No</th>
+                    <th style="border: 2px solid #111;padding: 11px;text-align: center;font-weight:900">Principal</th>
+                    <th style="border: 2px solid #111;padding: 11px;text-align: center;font-weight:900">Interest</th>
+                    <th style="border: 2px solid #111;padding: 11px;text-align: center;font-weight:900">Installment</th>
+                    <th style="border: 2px solid #111;padding: 11px;text-align: center;font-weight:900">Capital Repay</th>
+                    <th style="border: 2px solid #111;padding: 11px;text-align: center;font-weight:900">Balance</th>
+                </tr>
+            </thead>
+            <tbody>`;
+
+for (let i = 0; i < 10; i++) {
+    let curprincipal = i == 0 ? principal : Balance;
+    let curinterest = ctsUtils.calculateSimpleInterest(curprincipal, rate, time);
+    totalInterest +=curinterest;
+    totalInstallment +=Installment;
+    let repay = Installment - curinterest;
+    log.debug({ title: "balance", repay });
+
+    Balance = curprincipal - repay;
+
+    innerHTML += `<tr>
+        <td style="border: 2px solid #111;padding: 11px;text-align: center;font-weight:900">${i + 1}</td>
+        <td style="border: 2px solid #111;padding: 11px;text-align: center;">${curprincipal.toFixed(2)}</td>
+        <td style="border: 2px solid #111;padding: 11px;text-align: center;">${curinterest.toFixed(2)}</td>
+        <td style="border: 2px solid #111;padding: 11px;text-align: center;">${Installment.toFixed(2)}</td>
+        <td style="border: 2px solid #111;padding: 11px;text-align: center;">${repay.toFixed(2)}</td>
+        <td style="border: 2px solid #111;padding: 11px;text-align: center;">${Balance.toFixed(2)}</td>
+    </tr>`;
+}
+
+innerHTML += `
+ <td ></td>
+ <td ></td>
+ <td style="border: 2px solid #111;padding: 11px;text-align: center;">${totalInterest.toFixed(2)}</td>
+  <td style="border: 2px solid #111;padding: 11px;text-align: center;">${totalInstallment.toFixed(2)}</td>
+   <td style="border: 2px solid #111;padding: 11px;text-align: center;">${principal.toFixed(2)}</td></tbody>
+        </table>
+    </body>
+</html>`;
+
+        retValue.success = true;
+        retValue.data = innerHTML
+    }
+    catch (e) {
+        log.error({ title: 'Error - compute Simple Interest', details: e });
+        retValue.success = false;
+        retValue.message = e;
+    }
+    return retValue
 }
--- src/FileCabinet/SuiteScripts/sri-mos-si-sl-v1.js
+++ src/FileCabinet/SuiteScripts/sri-mos-si-sl-v1.js
@@ -3,26 +3,23 @@
  * @NScriptType Suitelet
  */
 /*
-Name        : Suitelet Demonstration
-Author      : Mosses Ross
-Description : Compute Simple Interest
-Dependencies: None
-Release Date: 2023-06-01
-version     : 1.0.0
-Changelog   : 1.0.0 - Initial release
-website     : www.cloudiotech.com
+Name         : Suitelet Demonstration
+Author       : Mosses Ross
+Description  : Compute Simple and Compound Interest
+Release Date : 2023-06-15
 */
 
-var query, record, runtime, serverWidget;
-var modules = ['N/query', 'N/record', 'N/runtime', 'N/ui/serverWidget'];
+var query, record, runtime, serverWidget, ctsUtils;
+var modules = ['N/query', 'N/record', 'N/runtime', 'N/ui/serverWidget', './sri-mos-pc-mod-v1'];
 
 define(modules, main);
 
-function main(queryModule, recordModule, runtimeModule, serverWidgetModule) {
+function main(queryModule, recordModule, runtimeModule, serverWidgetModule, ctsModule) {
     query = queryModule;
     record = recordModule;
     runtime = runtimeModule;
     serverWidget = serverWidgetModule;
+    ctsUtils = ctsModule;
     return {
         onRequest: onRequest
     }
@@ -31,148 +28,316 @@ function main(queryModule, recordModule, runtimeModule, serverWidgetModule) {
 function onRequest(scriptContext) {
     var retValue = { success: false, message: '', data: {} };
     try {
-        // Block 1 : Get Operation
         if (scriptContext.request.method == 'GET') {
-            retValue = createUI(scriptContext);
+            log.debug({ title: "scriptContext", details: scriptContext });
+            retValue = routeGet(scriptContext);
         }
-
-        // Block 2 : Post Operation
         if (scriptContext.request.method == 'POST') {
-            retValue = processPost(scriptContext);
+            retValue = routePost(scriptContext);
         }
-
-        // Block 3 : Response/Rendering
         if (retValue.success) {
             log.debug({ title: 'On Success', details: retValue });
             scriptContext.response.writePage(retValue.data.form);
+            // var responseText = `<html><body><h1 style="color: blue; text-align: center;">` + retValue.message + '</h1></body></html>';
+            // scriptContext.response.write(responseText);
         } else {
             retValue.message = retValue.message ? retValue.message : 'User Formatted errors';
-            var responseText = '<html><body><h1>' + retValue.message + '</h1></body></html>';
+            let responseText = "<html><body><h1>" + retValue.message + "</h1></body></html>";
             scriptContext.response.write(responseText);
         }
-
     } catch (e) {
         log.error({ title: 'Error - on request', details: e });
     }
 }
-
-function processPost(scriptContext) {
-    var retValue = { success: false, message: '', data: {} };
-    try {
-        // extract user Inputs
-        var userInputs = scriptContext.request.parameters;
-
-        var principal = userInputs.custpage_principal;
-        var rate = userInputs.custpage_rate;
-        var time = userInputs.custpage_time;
-
-        // convert and format input values
-        principal = principal ? principal : 0;
-        rate = rate ? rate : 0;
-        time = time ? time : 0;
-        principal = parseFloat(principal);
-        rate = parseFloat(rate);
-        time = parseInt(time);
-        log.debug({ title: 'User Inputs', details: principal + '|' + rate + '|' + time });
-
-        var interest = computeInterest(principal, rate, time);
-        log.debug({ title: 'Interest', details: interest });
-
-        var form = serverWidget.createForm({
-            title: 'Your Earnings'
-        });
-       var field_principal = form.addField({
-            id: 'custpage_principal',
-            type: serverWidget.FieldType.CURRENCY,
-            label: 'Principal'
-        })
-        field_principal.defaultValue = principal;
-
-        form.addField({
-            id: 'custpage_rate',
-            type: serverWidget.FieldType.PERCENT,
-            label: 'Rate of Interest'
-        }).defaultValue = rate;
-
-        form.addField({
-            id: 'custpage_time',
-            type: serverWidget.FieldType.INTEGER,
-            label: 'Time in Years'
-        }).defaultValue = time;
-        
-        var field_interest = form.addField({
-            id: 'custpage_simple_interest',
-            type: serverWidget.FieldType.CURRENCY,
-            label: 'Simple Interest',
-        });
-        field_interest.defaultValue = interest;
-        field_interest.updateDisplayType({displayType: serverWidget.FieldDisplayType.DISABLED});
-
-        form.addSubmitButton({
-            label: 'Calculate'
-        });
-        retValue.success = true;
-        retValue.message = 'Interest calculated successfully';
-        retValue.data = { form: form };
-    } catch (e) {
-        log.error({ title: 'Error - computeSimpleInterest', details: e });
-        retValue.success = false;
-        retValue.message = e;
-    }
+function routeGet(scriptContext) {
+    let myParams = { action: 'GET', data: {} }
+    myParams.data = null;
+    retValue = createUI(scriptContext, myParams);
     return retValue;
 }
 
-
-function computeInterest(principal, rate, time) {
-    return (principal * rate * time) / 100;
-}
-
-
-function createUI(scriptContext) {
+function createUI(scriptContext, myParams, type) {
     var retValue = { success: false, message: '', data: {} };
     try {
         var form = serverWidget.createForm({
-            title: 'Simple and Compound Interest Calculation'
+            title: 'EasyCalc Finance'
         });
-        form.addField({
-            id: 'custpage_principal',
-            type: serverWidget.FieldType.CURRENCY,
-            label: 'Principal'
+        let fldPrincipal = form.addField({ id: 'custpage_principle', type: serverWidget.FieldType.CURRENCY, label: 'Principal' });
+        let fldRate = form.addField({ id: 'custpage_rate', type: serverWidget.FieldType.PERCENT, label: "Rate of Interest" });
+        let fldTime = form.addField({ id: "custpage_time", type: serverWidget.FieldType.INTEGER, label: "Time in Years" });
+        var fldSimpleInterest = null;
+        if (type == 'SI') {
+            fldSimpleInterest = form.addField({ id: "custpage_simple_interest", type: serverWidget.FieldType.CURRENCY, label: "Simple Interest" }).updateDisplayType({ displayType: serverWidget.FieldDisplayType.DISABLED });
+
+        } else if (type == 'CI') {
+            fldSimpleInterest = form.addField({ id: "custpage_compound_interest", type: serverWidget.FieldType.CURRENCY, label: "Compound Interest" }).updateDisplayType({ displayType: serverWidget.FieldDisplayType.DISABLED });
+        }
+
+        // let fldEMI = form.addField({ id: "custpage_emi_calculation", type: serverWidget.FieldType.CURRENCY, label: "Compound Interest" });
+        // fldEMI.updateDisplayType({ displayType: serverWidget.FieldDisplayType.DISABLED });
+        let fldAction = form.addField({ id: 'custpage_action', type: serverWidget.FieldType.TEXT, label: 'Action' }).updateDisplayType({ displayType: serverWidget.FieldDisplayType.HIDDEN });;
+
+        if (myParams.action == 'POST') {
+            fldPrincipal.defaultValue = myParams.data.principal;
+            fldRate.defaultValue = myParams.data.rata;
+            fldTime.defaultValue = myParams.data.time;
+            fldSimpleInterest.defaultValue = myParams.data.interest.toFixed(2);
+            // fldEMI.defaultValue = myParams.data.emi;
+        }
+
+        form.addButton({
+            id: 'custpage_calc_si',
+            label: 'Calc Simple Interest',
+            functionName: 'setAction("SI")'
         });
-        form.addField({
-            id: 'custpage_rate',
-            type: serverWidget.FieldType.PERCENT,
-            label: 'Rate of Interest'
+
+        form.addButton({
+            id: 'custpage_calc_ci',
+            label: 'Calc Compound Interest',
+            functionName: 'setAction("CI")'
         });
-        form.addField({
-            id: 'custpage_time',
-            type: serverWidget.FieldType.INTEGER,
-            label: 'Time in Years'
+
+        form.addButton({
+            id: 'custpage_calc_emi',
+            label: 'Calculate EMI',
+            functionName: 'setAction("EMI")'
         });
-        form.addField({
-            id: 'custpage_simple_interest',
-            type: serverWidget.FieldType.CURRENCY,
-            label: 'Simple Interest'
-        }).updateDisplayType({
-            displayType: serverWidget.FieldDisplayType.DISABLED
-        });
-        form.addField({
-            id: 'custpage_compound_interest',
-            type: serverWidget.FieldType.CURRENCY,
-            label: 'Compound Interest'
-        }).updateDisplayType({
-            displayType: serverWidget.FieldDisplayType.DISABLED
-        });
-        form.addSubmitButton({
-            label: 'Calculate'
-        });
+
+        form.clientScriptModulePath = './cts-cad-finCalc-cs-v1.js'
+
         retValue.success = true;
         retValue.message = 'UI created successfully';
         retValue.data = { form: form };
     } catch (e) {
-        log.error({ title: 'User Thrown Error', details: e });
+        log.error({ title: 'Error - Create UI', details: e });
         retValue.success = false;
         retValue.message = e;
     }
     return retValue;
+}
+
+function routePost(scriptContext) {
+    let retValue = { success: false, message: "", data: {} }
+    try {
+        var userAction = scriptContext.request.parameters.custpage_action;
+
+        if (userAction == 'SI') {
+            retValue = processInterest(scriptContext, 'SI');
+        } else if (userAction == 'CI') {
+            retValue = processInterest(scriptContext, 'CI');
+        } else if (userAction == "EMI") {
+            retValue = processEMI(scriptContext);
+        }
+        return retValue;
+    }
+    catch (e) {
+        log.error({ title: 'Error - Process Post', details: e });
+        retValue.success = false;
+        retValue.message = e;
+
+    }
+    return retValue;
+}
+
+function processEMI(scriptContext) {
+    var retValue = { success: false, message: '', data: {} };
+    try {
+        log.debug({ title: "scriptContext", details: scriptContext });
+        userInput = scriptContext.request.parameters;
+
+        let principal = parseFloat(userInput.custpage_principle ? userInput.custpage_principle : 0);
+        let rate = parseFloat(userInput.custpage_rate ? userInput.custpage_rate : 0);
+        let time = parseInt(userInput.custpage_time ? userInput.custpage_time : 0);
+        let interest = ctsUtils.calculateSimpleInterest(principal, rate, time);
+        let calcEMI = ctsUtils.calculateEMI(principal, rate, time);
+        let EMI = calcEMI * 12;
+
+
+        let myParams = { action: 'POST', data: {} }
+        myParams.data = { principal: principal, rata: rate, time: time, interest: interest, emi: EMI }
+        retValue = createUIForEMI(scriptContext, myParams);
+    }
+    catch (e) {
+        log.error({ title: 'Error - compute Simple Interest', details: e });
+        retValue.success = false;
+        retValue.message = e;
+    }
+    return retValue
+}
+
+function createUIForEMI(scriptContext, myParams) {
+    var retValue = { success: false, message: '', data: {} };
+    try {
+        var form = serverWidget.createForm({
+            title: 'Creating a EMI Table'
+        });
+        var fieldgroup = form.addFieldGroup({
+            id: "fieldgroup_fin_input_data",
+            label: "User Input Value",
+        });
+
+        var fldPrincipal = form.addField({ id: 'custpage_principle', type: serverWidget.FieldType.CURRENCY, label: 'Principal',container:'fieldgroup_fin_input_data' });
+        var fldRate = form.addField({ id: 'custpage_rate', type: serverWidget.FieldType.PERCENT, label: "Rate of Interest" ,container:'fieldgroup_fin_input_data'});
+        var fldTime = form.addField({ id: "custpage_time", type: serverWidget.FieldType.INTEGER, label: "Time in Years" ,container:'fieldgroup_fin_input_data'});
+        fldPrincipal.defaultValue = myParams.data.principal;
+        fldRate.defaultValue = myParams.data.rata;
+        fldTime.defaultValue = myParams.data.time;
+
+        form.addSubtab({
+            id: "subtab_table",
+            label: "sublist Table",
+        });
+
+        var sublist = form.addSublist({
+            id: 'custpage_table',
+            type: serverWidget.SublistType.STATICLIST,
+            label: 'EMI Calculator',
+            tab:"subtab_table"
+        });
+
+        //Header
+        sublist.addField({
+            id: 'custpage_sn',
+            type: serverWidget.FieldType.INTEGER,
+            label: 'S.No'
+        });
+
+        sublist.addField({
+            id: 'custpage_principal',
+            type: serverWidget.FieldType.CURRENCY,
+            label: 'Principal'
+        });
+
+        sublist.addField({
+            id: 'custpage_interest',
+            type: serverWidget.FieldType.CURRENCY,
+            label: 'Interest'
+        });
+
+        sublist.addField({
+            id: 'custpage_installment',
+            type: serverWidget.FieldType.CURRENCY,
+            label: 'Installment'
+        });
+
+        sublist.addField({
+            id: 'custpage_capital_repay',
+            type: serverWidget.FieldType.CURRENCY,
+            label: 'Capital Repay'
+        });
+        sublist.addField({
+            id: 'custpage_balance',
+            type: serverWidget.FieldType.CURRENCY,
+            label: 'Balance'
+        });
+
+        // table values 
+        let principal = myParams.data.principal;
+        let rate = myParams.data.rata
+        let time = myParams.data.time
+        let interest = myParams.data.interest
+        let Installment = myParams.data.emi
+        var Balance = principal
+        var curprincipal = principal;
+        for (let i = 0; i < 10; i++) {
+            curprincipal = i == 0 ? principal : Balance;
+            let curinterest = ctsUtils.calculateSimpleInterest(curprincipal, rate, time);
+            let repay = Installment - curinterest
+            log.debug({ title: "balance", repay });
+           
+            Balance = curprincipal - repay
+            
+
+
+            // log.debug({ title: "curprincipal", Balance });
+
+            sublist.setSublistValue({
+                id: 'custpage_sn',
+                line: i,
+                value: i + 1
+            });
+
+            sublist.setSublistValue({
+                id: 'custpage_principal',
+                line: i,
+                value: curprincipal.toFixed(2)
+            });
+            sublist.setSublistValue({
+                id: 'custpage_interest',
+                line: i,
+                value: curinterest.toFixed(2)
+            });
+            sublist.setSublistValue({
+                id: 'custpage_installment',
+                line: i,
+                value: Installment.toFixed(2)
+            });
+            sublist.setSublistValue({
+                id: 'custpage_capital_repay',
+                line: i,
+                value: repay.toFixed(2)
+            });
+            sublist.setSublistValue({
+                id: 'custpage_balance',
+                line: i,
+                value: Balance.toFixed(2)
+            });
+
+        }
+        
+        var fieldgroup = form.addFieldGroup({
+            id: "fieldgroup_primary_details",
+            label: "HTML Table",
+        });
+        
+        let innerHTML=ctsUtils.GenerateHTML(scriptContext, myParams);
+        form.addField({
+            id: "custpage_html_table",
+            type: serverWidget.FieldType.INLINEHTML,
+            label: "HTML",
+            container: "fieldgroup_primary_details"
+          }).defaultValue = innerHTML.data;
+
+        retValue.success = true;
+        retValue.message = 'Table created successfully';
+        log.debug({ title: "form", details: form });
+        retValue.data = { form: form };
+    }
+    catch (e) {
+        log.error({ title: 'Error - Table', details: e });
+        retValue.success = false;
+        retValue.message = e;
+    }
+    return retValue;
+}
+
+
+
+function processInterest(scriptContext, type) {
+    var retValue = { success: false, message: '', data: {} };
+    try {
+        log.debug({ title: "scriptContext", details: scriptContext });
+        userInput = scriptContext.request.parameters;
+
+        let principal = parseFloat(userInput.custpage_principle ? userInput.custpage_principle : 0);
+        let rate = parseFloat(userInput.custpage_rate ? userInput.custpage_rate : 0);
+        let time = parseInt(userInput.custpage_time ? userInput.custpage_time : 0);
+        var interest = null;
+        if (type == 'SI') {
+            interest = ctsUtils.calculateSimpleInterest(principal, rate, time);
+        }
+        else if ((type == 'CI')) {
+            interest = ctsUtils.calculateCompoundInterest(principal, rate, time, 12);
+        }
+
+        let myParams = { action: 'POST', data: {} }
+        myParams.data = { principal: principal, rata: rate, time: time, interest: interest }
+        retValue = createUI(scriptContext, myParams, type);
+    }
+    catch (e) {
+        log.error({ title: 'Error - compute Simple Interest', details: e });
+        retValue.success = false;
+        retValue.message = e;
+    }
+    return retValue
 }
