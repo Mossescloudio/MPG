841b4f281745d7862b43808d1151a17a004d7f0e
Mosses <mosses@cloudiotech.com>
2025-01-02 12:41:44 +0530

    First Commit for CSDTM

---
+++ src/FileCabinet/SuiteScripts/csdtm1-cs-v1.js
@@ -0,0 +1,33 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+
+/*
+Name          : Client Script
+Author        : Mosses
+Description   : For my test game
+Dependencies  : None
+Release Date  : 2024-11-20
+Version       : 1.0.0
+Changing      : 1.0.0 - Initial release
+Website       : www.cloudio.com
+*/
+var clientRecord;
+var module = ["N/currentRecord"];
+
+define(module, main);
+
+function main(recordModule) {
+  clientRecord = recordModule;
+  return { setAction: setAction };
+}
+
+function setAction(action) {
+  var curRecord = clientRecord.get();
+  curRecord.setValue({ fieldId: 'custpage_action', value: action });
+  document.forms[0].submit();
+}
+
+
